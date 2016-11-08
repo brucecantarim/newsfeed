b76ab2cb8b7584027b72f0889e5d1b7c8a00a2e4
brucecantarim <bruce@cantarim.com>
2016-11-08 21:40:19 -0200

    Loading implementation
    Now the user see a loading message while the app is working.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,9 +2,9 @@
 
 // Declaration Block
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular'; // Adding loading, for feedback
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map'; // from reactiveX, for Observable Array conversion (Only works with this type!)
+import 'rxjs/add/operator/map'; // From reactiveX, for Observable Array conversion (Only works with this type!)
 
 // Decoration Block
 @Component({
@@ -18,14 +18,32 @@ export class HomePage {
     public feeds: Array<string>; // This is where the feed addresses will be stored
     private url: string = "https://www.reddit.com/new.json"; // Calling reddit feed
 
-    constructor(public navCtrl: NavController, public http: Http) { // Instantiating the classes
+    constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) { // Instantiating the classes
         
+        this.fetchContent();
+        
+    }
+    
+    fetchContent():void {
+        
+        // Initializing the loading
+        let loading = this.loadingCtrl.create({
+            content: 'Fetching content...' // Here we define the message displayed to the user
+            });
+        
+        // Presenting the loading message to the user
+        loading.present();
+
         // Since we injected Http component here, we can use 'this' when calling it
         this.http.get(this.url).map(res => res.json())
             .subscribe(data => {
                 this.feeds = data.data.children; // Sending the converted result back to the feeds Array
-            });
+            
+            // Loading is finish, so let's kill the messenger
+            loading.dismiss();
+                
+        });
     
-  }
+    }
 
 }
